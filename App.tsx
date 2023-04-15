1b19a5c7743c708750e18d986b8c759d84f2a8c4
haruvathsan <57910805+haruvathsan@users.noreply.github.com>
2023-04-15 12:15:04 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import Section from './Section';
+import Banner from './banner';
+
+export default function App() {
+  return (
+    <div>
+    <Section />
+
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-dbeltw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-dbeltw)
